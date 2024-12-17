8ca2ea457557733c119e3c567eb6f8081792ca46
tesddev <tesleem.amuda@gmail.com>
2024-12-17 23:16:57 +0100

    add new post end points

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const authenticateToken = require('../middlewares/auth');
+const { authenticateToken, isAuthorized } = require('../middlewares/auth');
 const authorizeRoles = require('../middlewares/role');
 const postRouter = express.Router();
-const { getPost, createPost, getAllPosts } = require('../controllers/postController');
+const { getPost, createPost, getAllPosts, editPost, deletePost } = require('../controllers/postController');
 
 postRouter.post('/createPost', authenticateToken, authorizeRoles('Author', 'Admin'), createPost);
 postRouter.get('/posts', getAllPosts);
 postRouter.get('/posts/:id', getPost);
+postRouter.put('/posts/:id', authenticateToken, isAuthorized, editPost);
+postRouter.delete('/posts/:id', authenticateToken, isAuthorized, deletePost);
 
 module.exports = postRouter;
