8973b76d3c170408c589cbaf56ff9b1cc1b7d178
tesddev <tesleem.amuda@gmail.com>
2024-12-17 23:16:04 +0100

    add new end points

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authRouter = express.Router();
-const { register, login } = require('../controllers/authController')
+const { register, login, requestPasswordReset, resetPassword } = require('../controllers/authController')
 
 authRouter.post('/register', register);
 authRouter.post('/login', login);
+authRouter.post('/request-password-reset', requestPasswordReset);
+authRouter.post('/reset-password/:token', resetPassword);
 
 module.exports = authRouter;
