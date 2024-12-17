45968ab967c42573265aa553a16f89fb2d7d33c2
tesddev <tesleem.amuda@gmail.com>
2024-12-18 00:31:22 +0100

    use the delete comment method

--- routes/comments.js
+++ routes/comments.js
@@ -2,8 +2,9 @@ const express = require('express');
 const commentRouter = express.Router();
 const { authenticateToken, isAuthorized } = require('../middlewares/auth');
 const authorizeRoles = require('../middlewares/role');
-const postComment = require('../controllers/commentController');
+const {postComment, deleteComment} = require('../controllers/commentController');
 
 commentRouter.post('/postComment', authenticateToken, postComment);
+commentRouter.delete('/deleteComment/:id', authenticateToken, authorizeRoles('Admin'), deleteComment);
 
 module.exports = commentRouter;
