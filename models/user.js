3118347b773d41eedd15b796fef1148597bab515
tesddev <tesleem.amuda@gmail.com>
2024-12-17 23:15:44 +0100

    update user schema to accomodate nodemailer for password reset

--- models/user.js
+++ models/user.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['Admin', 'Author', 'Reader'], default: 'Reader' },
+    resetToken: { type: String },
+    resetTokenExpires: {type: Date },
 });
 
-UserSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
+UserSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.resetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    this.resetTokenExpires = Date.now() + 10 * 60 * 1000;
+    return resetToken; 
+};
 
 module.exports = mongoose.model('User', UserSchema);
